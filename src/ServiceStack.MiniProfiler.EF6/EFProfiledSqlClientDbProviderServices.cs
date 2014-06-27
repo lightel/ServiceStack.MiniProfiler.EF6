242e8ecec6db519d6f238348fb379b3a420e46d3
Alexey Andrushkevich <aandrushkevich@mh1889.com>
2014-06-28 00:26:58 +0300

    Initial commit

---
+++ src/ServiceStack.MiniProfiler.EF6/EFProfiledSqlClientDbProviderServices.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceStack.MiniProfiler.EF6
+{
+    using System.Data.Entity.SqlServer;
+
+    /// <summary>
+    /// Specific implementation of <c>EFProfiledDbProviderFactory&lt;SqlClientFactory&gt;</c> to enable profiling
+    /// </summary>
+    public class EFProfiledSqlClientDbProviderServices
+        : EFProfiledDbProviderServices<SqlProviderServices>
+    {
+        /// <summary>
+        /// Every provider factory must have an Instance public field
+        /// </summary>
+        public static new EFProfiledSqlClientDbProviderServices Instance = new EFProfiledSqlClientDbProviderServices();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="EFProfiledSqlClientDbProviderServices"/> class from being created.
+        /// </summary>
+        private EFProfiledSqlClientDbProviderServices()
+        {
+        }
+    }
+}
